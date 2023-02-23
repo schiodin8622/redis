58f94fb1bd42be2fd8814964ed8ed947b806da0b
schiodin8622 <s_chiodin@yahoo.com.ar>
2023-02-22 22:45:32 -0300

    upload1

---
+++ src/main/java/com/redis/isch/IschApplication.java
@@ -0,0 +1,13 @@
+package com.redis.isch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IschApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IschApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redis/isch/IschApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.isch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IschApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
